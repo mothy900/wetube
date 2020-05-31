21955f116d66c2bd719a6cb502a919742259060a
mothy900 <mothy900@naver.com>
2020-05-31 21:22:00 +0900

    using async, await, error try catch, : JS is  waiting for video info and make video info

--- controller/videoController.js
+++ controller/videoController.js
@@ -1,6 +1,16 @@
 import routes from "../routes";
-export const home = (req, res) => {
-  res.render("home", { pageTitle: "Home", videos });
+import Video from "../models/Video";
+export const home = async (req, res) => {
+  //async는 javascript에게 이 함수는 반드시 기다려야 된다고 알려주는 함수.
+  try {
+    const videos = await Video.find({}); // 모든 video를 기다려야 한다. await 는 async에서만 사용 가능한 명령어
+
+    res.render("home", { pageTitle: "Home", videos });
+  } catch (error) {
+    console.log(error);
+    res.render("home", { pageTitle: "Home", videos: [] });
+  }
+  //console.log(videos);
 };
 
 export const search = (req, res) => {
--- init.js
+++ init.js
@@ -3,8 +3,9 @@ import app from "./app";
 import dotenv from "dotenv";
 dotenv.config();
 import "./models/Video";
+import "./models/Comment";
 
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 7000;
 
 const handleListening = () =>
   console.log(`Listening on: http://localhost:${PORT}`);
